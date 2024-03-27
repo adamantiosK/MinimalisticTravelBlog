5a28c2d327cc947b0ffec4d7b1fc2a7f9630c6a0
Adamantios Kounis <adamantioschios@gmail.com>
2024-03-27 16:36:47 +0100

    initial home page done. Integrated with online db. awaiting additional feautures. including individual page for post stories.

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import HorizontalScroll from './component/HorizontalScroll';
 const supabase = createClient(import.meta.env.VITE_API_BASE_URL, import.meta.env.VITE_SUPABASE_DB_KEY);
 
 function App() {
-  const [count, setCount] = useState(0);
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
@@ -18,38 +17,24 @@ function App() {
   }, []);
 
   async function getPosts() {
-    const { data } = await supabase.from("Posts").select();
-    setPosts(data);
+    const { data, error } = await supabase
+      .from("Posts")
+      .select()
+      .order("created_at", { ascending: false }); // Order by 'created_at' in descending order
+  
+    if (data) {
+      setPosts(data);
+    }
+  
+    if (error) {
+      console.error("Error fetching posts:", error.message);
+    }
   }
-
-
 
   return (
     <>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <ul>
-          {posts.map((post) => (
-            <li key={post.id}>{post.title}</li>
-          ))}
-        </ul>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div> */}
       <div className="bg-peach-100">
-        <HorizontalScroll />
+        <HorizontalScroll posts={posts} />
       </div>
     </>
   )
--- src/component/HorizontalScroll.jsx
+++ src/component/HorizontalScroll.jsx
@@ -8,7 +8,7 @@ import {
 } from "framer-motion";
 import React, { useRef } from "react";
 
-const HorizontalScroll = () => {
+const HorizontalScroll = ({ posts }) => {
     const targetRef = useRef(null);
     const { scrollYProgress } = useScroll({
         target: targetRef,
@@ -24,12 +24,11 @@ const HorizontalScroll = () => {
                         <h2 className="text-9xl font-bold text-white font-pacifico">Every</h2>
                         <h3 className="text-8xl font-bold text-white font-pacifico">Day</h3>
                         <h2 className="text-9xl font-bold text-white font-pacifico">Norm</h2>
-                        <p className="text-xl font-light text-white mt-12 font-pacifico">The minimalistic blog</p>
-                        {/* Animated Scroll Icon */}
+                        <p className="text-xl font-light text-white mt-12 font-pacifico">The minimalistic travel blog</p>
                         <i className="fas fa-chevron-down text-white text-3xl animate-bounce mt-8"></i>
                     </div>
-                    {cards.map((card) => {
-                        return <Card card={card} key={card.id} />;
+                    {posts.map((post) => {
+                        return <Card post={post} key={post.id} />;
                     })}
                 </motion.div>
             </div>
@@ -37,15 +36,15 @@ const HorizontalScroll = () => {
     );
 };
 
-const Card = ({ card }) => {
+const Card = ({ post }) => {
     return (
         <div
-            key={card.id}
+            key={post.id}
             className="group relative h-[40rem] w-[29rem] rounded-xl bg-gradient-to-br from-purple-400 to-blue-700 mr-12"
         >
             <div
                 style={{
-                    backgroundImage: `url(${card.url})`,
+                    backgroundImage: `url(${post.img_url ?? ""})`,
                     backgroundSize: "cover",
                     backgroundPosition: "center",
                     opacity: 0.9
@@ -57,13 +56,13 @@ const Card = ({ card }) => {
                     <div className="absolute inset-0 bg-gradient-to-br from-white/20 to-white/0 backdrop-blur-lg rounded-lg opacity-70"></div>
                     <div className="relative p-8 text-center">
                         <p className="text-5xl font-black uppercase text-black">
-                            {card.title}
+                            {post.title}
                         </p>
                         <p className="text-4xl font-bold uppercase text-black">
-                            {card.title}
+                            {post.created_at}
                         </p>
                         <p className="text-3xl font-semibold uppercase text-black italic">
-                            {card.title}
+                            {post.author}
                         </p>
                     </div>
                 </div>
@@ -74,41 +73,3 @@ const Card = ({ card }) => {
 
 
 export default HorizontalScroll;
-
-const cards = [
-    {
-        url: "./src/assets/img/BotanicalGardens.JPG",
-        title: "Georgia",
-        id: 1,
-    },
-    {
-        url: "./src/assets/img/OnBoatWave.JPG",
-        title: "Swizzerland",
-        id: 2,
-    },
-    {
-        url: "./src/assets/img/MaltaPool.JPG",
-        title: "Malta",
-        id: 3,
-    },
-    {
-        url: "./src/assets/img/SnowBoard.jpg",
-        title: "France",
-        id: 4,
-    },
-    {
-        url: "./src/assets/img/BotanicalGardens.JPG",
-        title: "Georgia",
-        id: 5,
-    },
-    {
-        url: "./src/assets/img/OnBoatWave.JPG",
-        title: "Swizzerland",
-        id: 6,
-    },
-    {
-        url: "./src/assets/img/MaltaPool.JPG",
-        title: "Malta",
-        id: 7,
-    },
-];
